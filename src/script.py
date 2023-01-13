37f24180fdf7b833b2cbe89ffa51dab1965f39e5
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-01-13 15:43:22 -0500

    update cli parsing

--- src/script.py
+++ src/script.py
@@ -1,8 +1,10 @@
 # FILE TO BUILD CLONE OF "https://github.com/timm/tested/blob/main/src/script.lua"
+import sys
 
 K_HELP = 'help'
 K_SEED = 'seed'
 K_DEFAULT_SEED_VALUE = 937162211
+K_TEST = 'test'
 
 global_options = {}
 
@@ -10,26 +12,26 @@ class Num:
     def __init__(self):
         print("TODO - fill the constructor")
     
-    def add(value):
+    def add(self, value):
         print("TODO - fill adding value to the num class")
 
-    def mid():
+    def mid(self):
         print("TODO - get mid value")
     
-    def div():
+    def div(self):
         print("TODO - implement standard deviation using Wolford's algorithm")
 
 class Sym:
     def __init__(self):
         print("TODO - fill the constructor for Sym")
 
-    def add(value):
+    def add(self, value):
         print("TODO - fill add symbol value")
     
-    def mid():
+    def mid(self):
         print("TODO - return the mode value")
 
-    def div():
+    def div(self):
         print("TODO - return the standard entropy")
 
 
@@ -45,11 +47,55 @@ def rand_int(n):
     # return int value
     print("TODO - return random number using rand")
 
-def initialize_from_cli():
-    print("TODO - add cli parsing and initialization")
+def print_help():
+    print('''
+    OPTIONS:
+    -h or --help            -> Show this message.
+    -s or --seed            -> Set seed value for random number generator.
+    ''')
 
+def handle_unknown_cli_option():
+    print("Unknown option, please run -h (or) --help for more details.")
+
+def get_option_key_and_value_requirement(key) -> tuple[str, bool]:
+    if key == '-h' or key ==  K_HELP:
+        return (K_HELP, False)
+    elif key == '-s' or key == K_SEED:
+        return (K_SEED, True)
+    elif key == '-t' or key == K_TEST:
+        return (K_TEST, True)
+    else:
+        return (K_HELP, False)
+
+def parse_cli_options():
+    print("TODO - parse cli options")
+    print("initial cli : ", sys.argv)
+    # skip 0 for script name.
+    next_arg_is_value = False
+    option_key = ""
+    for arg in sys.argv[1:]:
+        if not next_arg_is_value:
+            option_details = get_option_key_and_value_requirement(arg)
+            if option_details[1]:
+                next_arg_is_value = True
+                option_key = option_details[0]
+            else:
+                # Options which do not require value might expect different handling.
+                if option_details[0] == K_HELP:
+                    print_help()
+                    return
+        else:
+            global_options[option_key] = arg
+            next_arg_is_value = False
+    print("Final options status : " , global_options)
+
+def default_cli_options():
     # initialize default seed value 
     global_options[K_SEED] = K_DEFAULT_SEED_VALUE
+
+def initialize_from_cli():
+    default_cli_options()
+    parse_cli_options()
 
 #### MAIN
 def __main__():
