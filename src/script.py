ad6cba0a8c58a65056580134750da15a2d80786c
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-01-13 13:50:43 -0500

    add basic structure with todos

--- src/script.py
+++ src/script.py
@@ -1,8 +1,51 @@
 # FILE TO BUILD CLONE OF "https://github.com/timm/tested/blob/main/src/script.lua"
 
+global_options = {}
 
+class Num:
+    def __init__(self):
+        print("TODO - fill the constructor")
+    
+    def add(value):
+        print("TODO - fill adding value to the num class")
+
+    def mid():
+        print("TODO - get mid value")
+    
+    def div():
+        print("TODO - implement standard deviation using Wolford's algorithm")
+
+class Sym:
+    def __init__(self):
+        print("TODO - fill the constructor for Sym")
+
+    def add(value):
+        print("TODO - fill add symbol value")
+    
+    def mid():
+        print("TODO - return the mode value")
+
+    def div():
+        print("TODO - return the standard entropy")
+
+
+def rand(low, hi):
+    # return float value
+    print("TODO - return random number between lo and hi")
+
+def rand_n(n, nPlaces):
+    # return float value
+    print("TODO - return random number with precision 'n'")
+
+def rand_int(n):
+    # return int value
+    print("TODO - return random number using rand")
+
+#### MAIN
 def __main__:
-    print("start here!")
-
+    # TODO:
+    # 1. build cli options
+    # 2. start up main functions 
+    # 3. test framework
 
 __main__()
