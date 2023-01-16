4cc97006bd65d57b1869ee3ea186cdd1a94fd3cb
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-01-16 17:14:51 -0500

    update test setup

--- src/test.py
+++ src/test.py
@@ -1,1 +1,19 @@
 from Sym import Sym
+from Num import Num
+
+def test_global_options() -> tuple[bool, str]:
+    return_value = (True, "")
+    return return_value
+
+def test_rand() -> tuple[bool, str]:
+    return_value = (True, "No error")
+    # TEST BODY
+    return return_value
+
+def test_num() -> tuple[bool, str]:
+    return_value = (True, "")
+    return return_value
+
+def test_sym() -> tuple[bool, str]:
+    return_value = (False, "")
+    return return_value
