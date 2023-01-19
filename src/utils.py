7ec3e6cce28b2cdc0f474828ce54d63336dbaf96
sushanthreddyC <sushanth1887@gmail.com>
2023-01-18 20:05:44 -0500

    fix rand

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import math
 
+from config import global_options, K_SEED, K_DEFAULT_SEED_VALUE
+
 def rnd(n, nPlaces = 3):
     """
     Rounds of the output to nPlaces places.
@@ -9,14 +11,13 @@ def rnd(n, nPlaces = 3):
     # return math.floor(n*mult)/mult
     return math.floor(n*mult+0.5)/mult
 
-seed = 937162211
-
 def rand( lo = 0, hi=1, default_seed=math.inf):
-  global seed
+  global global_options
+  global_options[K_SEED] = int(global_options[K_SEED])
   if default_seed != math.inf:
-    seed = default_seed 
-  seed = (16807*seed) % 2147483647
-  return lo+(hi-lo)*seed/2147483647
+    global_options[K_SEED] = default_seed 
+  global_options[K_SEED] = (16807*global_options[K_SEED]) % 2147483647
+  return lo+(hi-lo)*global_options[K_SEED]/2147483647
 
 def rint(lo, hi):
   return math.floor(0.5+rand(lo,hi))
