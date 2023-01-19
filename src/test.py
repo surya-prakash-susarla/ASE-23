b4b8834177ab137f8869d5fcaa1bb574cad2906e
sushanthreddyC <sushanth1887@gmail.com>
2023-01-18 19:57:35 -0500

    fixed - temp

--- src/test.py
+++ src/test.py
@@ -7,12 +7,17 @@ def test_global_options() -> bool:
 def test_rand() -> bool:
     num1 = Num()
     num2 = Num()
+    num1.add(rand(0,1,937162211))
     for i in range(1000):
-        num1.add(rand(937162211,0,1))
+        num1.add(rand(0,1))
+    num2.add(rand(0,1,937162211))
     for i in range(1000):
-        num2.add(rand(937162211,0,1))
+        num2.add(rand(0,1))
     m1= rnd(num1.mid(),10)
     m2= rnd(num2.mid(),10)
+    print("m1 : ", m1)
+    print("m2 : ", m2)
+    print(rnd(m1,1))
     return m1==m2 and .5 == rnd(m1,1)
 
 def test_num() -> bool:
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import math
+
 def rnd(n, nPlaces = 3):
     """
     Rounds of the output to nPlaces places.
@@ -8,8 +9,13 @@ def rnd(n, nPlaces = 3):
     # return math.floor(n*mult)/mult
     return math.floor(n*mult+0.5)/mult
 
-def rand(seed = 937162211, lo = 0, hi=1):
-  seed = 16807*seed % 2147483647
+seed = 937162211
+
+def rand( lo = 0, hi=1, default_seed=math.inf):
+  global seed
+  if default_seed != math.inf:
+    seed = default_seed 
+  seed = (16807*seed) % 2147483647
   return lo+(hi-lo)*seed/2147483647
 
 def rint(lo, hi):
