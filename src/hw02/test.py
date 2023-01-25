086f4715360c5129b2457a8defb4a745842993bf
psvkaushik <psvkaushik@gmail.com>
2023-01-24 22:27:09 -0500

    adding test

--- src/hw02/test.py
+++ src/hw02/test.py
@@ -1,21 +1,12 @@
 from sym import Sym
 from num import Num
 from utils import rand ,rint, rnd
+from data import stats
+from globals import global_options, K_FILE
+from data import Data
+
 def test_global_options() -> bool:
     return True
-
-def test_rand() -> bool:
-    num1 = Num()
-    num2 = Num()
-    num1.add(rand(0,1,937162211))
-    for i in range(1000):
-        num1.add(rand(0,1))
-    num2.add(rand(0,1,937162211))
-    for i in range(1000):
-        num2.add(rand(0,1))
-    m1= rnd(num1.mid(),10)
-    m2= rnd(num2.mid(),10)
-    return m1==m2 and .5 == rnd(m1,1)
 
 def test_num() -> bool:
     num=Num()
@@ -31,3 +22,34 @@ def test_sym() -> bool:
     for s in symbols:
         sym.add(s)
     return ("a"==sym.mid() and 1.379 == rnd(sym.div()))
+
+def get_stats():
+    
+    data = Data(global_options[K_FILE])
+    x_div={}
+    x_mid={}
+    y_div={}
+    y_mid={}
+
+    for col in data.cols.x:
+        #x_mid.append(stats(2,col,True))
+        mid_temp = stats(2, col, True)
+        x_mid[mid_temp[0]] = mid_temp[1]
+        #x_div.append(stats(2,col,False))
+        div_temp = stats(2, col, False)
+        x_div[div_temp[0]] = div_temp[1]
+    
+    for col in data.cols.y :
+        #y_mid.append(stats(2,col,True))
+        #y_div.append(stats(2,col,False))
+        mid_temp = stats(2, col, True)
+        y_mid[mid_temp[0]] = mid_temp[1]
+        div_temp = stats(2, col, False)
+        y_div[div_temp[0]] = div_temp[1]
+
+    print("x")
+    print(f"x \t mid \t {x_mid}")
+    print(f"  \t div \t {x_div}")
+    print("y")
+    print(f"y \t mid \t {y_mid}")
+    print(f"  \t div \t {y_div}")
