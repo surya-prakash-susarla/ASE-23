5bd7cc53e59faa4bff16de5759fbe5c3261f9c5a
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-02-03 10:08:32 -0500

    chagne condition in clustering condition

--- src/hw04/data.py
+++ src/hw04/data.py
@@ -119,10 +119,13 @@ class Data:
             cols = self.cols.x
         node = Node()
         node.data = self.clone(rows)
-        if len(rows) > 2*min:
+        print("len of rows : ", len(node.data.rows))
+        if len(rows) >= 2:
             left, right, node.A, node.B, node.mid,c = self.half(rows, cols, above)
             node.left  = self.cluster(left,  min, cols, node.A)
             node.right = self.cluster(right, min, cols, node.B)
+        else:
+            print("returning here")
         return node
 
     def sway(self, rows=None,min=None,cols=None,above=None):
