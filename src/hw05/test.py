e75839e618bd6826b6058b14a3189c93f88d118d
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-02-11 17:06:25 -0500

    remove extra tests

--- src/hw05/test.py
+++ src/hw05/test.py
@@ -1,7 +1,7 @@
 from sym import Sym
 from num import Num
 from utils import rand ,rint, rnd, show, get_repgrid_file_contents
-from data import Data, rep_cols , rep_rows, rep_grid, rep_place, transpose, person_1,person_2,person_3
+from data import Data, rep_cols , rep_rows, rep_grid, rep_place, transpose
 from csv import get_csv_rows
 from globals import global_options, K_FILE, K_DEFAULT_DATA_FILE
 from collections import OrderedDict
@@ -10,6 +10,38 @@ import copy
 
 def test_global_options() -> bool:
     print(global_options)
+    return True
+
+def test_rand() -> bool:
+    print("TODO - implement test for rand")
+    return True
+
+def test_some() -> bool:
+    print("TODO - implement test for some")
+    return True
+
+def test_clone() -> bool:
+    print("TODO - implement test for clone")
+    return True
+
+def test_dist() -> bool:
+    print("TODO - implement test for dist")
+    return True
+
+def test_cliffs() -> bool:
+    print("TODO - implement test for cliffs")
+    return True
+
+def test_tree() -> bool:
+    print("TODO - implement test for tree")
+    return True
+
+def test_sway() -> bool:
+    print("TODO - implement test for sway")
+    return True
+
+def test_bins() -> bool:
+    print("TODO - implement test for bins")
     return True
 
 def test_num() -> bool:
@@ -26,69 +58,11 @@ def test_sym() -> bool:
         sym.add(s)
     return ("a"==sym.mid() and 1.379 == rnd(sym.div()))
 
-def test_get_stats():
-    data = Data(global_options[K_FILE])
-    x_div={}
-    x_mid={}
-    y_div={}
-    y_mid={}
-    
-    for col in data.cols.x:
-        col = [col]
-        mid_temp = data.stats(2, col, True)
-        x_mid[mid_temp[0][0]] = mid_temp[0][1]
-        div_temp = data.stats(2, col, False)
-        x_div[div_temp[0][0]] = div_temp[0][1]
-
-    for col in data.cols.y :
-        col = [col]
-        mid_temp = data.stats(2, col, True)
-        y_mid[mid_temp[0][0]] = mid_temp[0][1]
-        div_temp = data.stats(2, col, False)
-        y_div[div_temp[0][0]] = div_temp[0][1]
-    x_mid = OrderedDict(sorted(x_mid.items()))
-    x_div = OrderedDict(sorted(x_div.items()))
-    x_mid_p = {}
-    x_div_p = {}
-    for i in x_mid:
-        x_mid_p[i] = x_mid[i]
-        x_div_p[i] = x_div[i]
-    y_mid = OrderedDict(sorted(y_mid.items()))
-    y_div = OrderedDict(sorted(y_div.items()))
-    y_mid_p = {}
-    y_div_p = {}
-    for i in y_mid:
-        y_mid_p[i] = y_mid[i]
-        y_div_p[i] = y_div[i]
-    print("x")
-    print(f"x \t mid \t {x_mid_p}")
-    print(f"  \t div \t {x_div_p}")
-    print("y")
-    print(f"y \t mid \t {y_mid_p}")
-    print(f"  \t div \t {y_div_p}")
-
-    return True
-
-
 def test_read_from_csv():    
     rows = get_csv_rows(global_options[K_FILE])
     total_rows = len(rows)
     total_columns = len(rows[0])
     return total_columns*total_rows==8*399
-
-def test_read_data_csv():
-    data = Data(global_options[K_FILE])
-    return (len(data.rows)==398) and (data.cols.x[0].at==0) and (len(data.cols.x)==4) and (data.cols.y[0].wt==-1) 
-
-def test_around():
-    data = Data(global_options[K_FILE])
-    print("0   0 ", data.rows[0])
-    A = data.around(data.rows[0])
-    
-    for i in range(len(A)):
-        if (i+1)%50 == 0 :
-            print(i+1, rnd( data.dist(A[i],data.rows[0]),2), A[i])
-    return True
 
 def test_half():
     data = Data(global_options[K_FILE])
@@ -98,81 +72,4 @@ def test_half():
     print(mid)
     print(B)
     return True
-
-def test_cluster():
-    """ N-Level bi-Clustering """
-    data = Data(global_options[K_FILE])
-    show(data.cluster(), cols =  data.cols.y, nPlaces = 1, is_mid = True)
-    return True
-
-def test_optimize():
-    """ Semi-Supervised Optimization"""
-    data = Data(global_options[K_FILE])
-    show(data.sway(), cols = data.cols.y, nPlaces = 1, is_mid = True)
-    return True
-
-def test_copy():
-    table_1 = dict(a=1, b=dict(c=2, d=[3]))
-    table_2 = copy.deepcopy(table_1)
-    table_2['b']['d'][0]=1000
-    print("before: ", table_1)
-    print("after: ", table_2)
-    return True
-
-def test_rep_cols():
-    contents = get_repgrid_file_contents(global_options[K_FILE])
-    rep_data = rep_cols(contents['cols'])
-    rep_data.cols.print_cols()
-    for row in rep_data.rows:
-        print(row.cells)
-    return True
-
-def test_synonyms():
-    contents = get_repgrid_file_contents(global_options[K_FILE])
-    rep_data = rep_cols(contents['cols'])
-    show(rep_data.cluster(), rep_data.cols, nPlaces=1)
-    return True
-
-def test_rep_rows():
-    contents = get_repgrid_file_contents(global_options[K_FILE])
-    data = rep_rows(contents, transpose(contents['cols']))
-    data.cols.print_cols()
-    for row in data.rows:
-        print(row.cells)
-    return True
-    
-def test_prototypes():
-
-    contents = get_repgrid_file_contents(global_options[K_FILE])
-    rows = rep_rows(contents, transpose(contents['cols']))
-    show(rows.cluster(), rows.cols, nPlaces=1)
-    return True
-
-
-def test_position():
-    contents = get_repgrid_file_contents(global_options[K_FILE])
-    rows = rep_rows(contents, transpose(contents['cols']))
-    rows.cluster()
-    rep_place(rows)
-    return True
-
-def test_every():
-    rep_grid(K_DEFAULT_DATA_FILE)
-    return True
-
-def custom_test():
-
-    print("**********PERSON1****************")
-    rows = person_1()
-    show(rows.cluster(), rows.cols, nPlaces=1)
-
-    print("**********PERSON2****************")
-    rows = person_2()
-    show(rows.cluster(), rows.cols, nPlaces=1)
-    print("**********PERSON3****************")
-    rows = person_3()
-    show(rows.cluster(), rows.cols, nPlaces=1)
-
-    return True
-
 
