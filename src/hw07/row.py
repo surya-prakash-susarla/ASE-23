09c4d628af7fb72faec1a535ffe81c464c861426
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-03-17 16:52:41 -0400

    remove original files

--- src/hw07/row.py
+++
@@ -1,8 +0,0 @@
-class Row:
-    def __init__(self, contents):
-        if type(contents) is not list:
-            raise Exception("Row being initialized without list, contents : ", contents)
-        self.cells = contents
-        self.x = None
-        self.y = None
-
